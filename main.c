ab82dbb165b2f181044d7a57ae038e8f00914b8e
Youness Abtaouri <yabtaour@e3r4p2.1337.ma>
2022-08-28 15:05:20 +0100

    done with checking arguments number and .cub

--- main.c
+++ main.c
@@ -3,18 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rsaf <rsaf@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: yabtaour <yabtaour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 20:51:34 by rsaf              #+#    #+#             */
-/*   Updated: 2022/08/27 03:38:03 by rsaf             ###   ########.fr       */
+/*   Updated: 2022/08/28 15:03:03 by yabtaour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./LIB/cub3d.h"
+#include "./includes/cub3d.h"
 
 int main(int argc, char **argv)
 {
-    if (argc == 1)
-        printf("%s : Error: No Map As Input\n", argv[0]);
+    if (argc != 2)
+    {
+        printf("Invalid number of arguments !!\n");
+        return (1);
+    }
+    if (!ft_check_cub(argv[1]))
+    {
+        printf("Invalid name of the map !!\n");
+        return (1);
+    }
     return (0);
 }
