4f4e54fc23fc1b2b878033feca8bc1a172f1f7a1
Youness Abtaouri <yabtaour@e3r4p3.1337.ma>
2022-08-31 14:22:15 +0100

    fields check is dynamic now

--- parsing/get_colors.c
+++ parsing/get_colors.c
@@ -6,12 +6,13 @@
 /*   By: yabtaour <yabtaour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 10:38:38 by yabtaour          #+#    #+#             */
-/*   Updated: 2022/08/31 14:05:37 by yabtaour         ###   ########.fr       */
+/*   Updated: 2022/08/31 14:20:27 by yabtaour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
+//this function checks how many commas are there in the color
 void	ft_check_comma(char *line)
 {
 	int	i;
@@ -29,24 +30,26 @@ void	ft_check_comma(char *line)
 		exit (ft_print_error("- Colors configuration is not correct"));
 }
 
-void	ft_get_colors(t_data *data)
+//this function cheks how many fields are there in the color
+void	ft_check_fields(char **fields)
 {
 	int	i;
 
+	i = 0;
+	while (fields[i])
+		i++;
+	if (i != 3)
+		exit (ft_print_error("- Colors configuration is not correct"));
+}
+
+void	ft_get_colors(t_data *data)
+{
 	ft_check_comma(data->sides.c_txt);
 	ft_check_comma(data->sides.f_txt);
 	data->floor.splited = ft_split(data->sides.f_txt, ',');
 	data->ceiling.splited = ft_split(data->sides.c_txt, ',');
 	free(data->sides.c_txt);
 	free(data->sides.f_txt);
-	i = 0;
-	while (data->floor.splited[i])
-		i++;
-	if (i != 3)
-		exit (ft_print_error("- Colors configuration is not correct"));
-	i = 0;
-	while (data->ceiling.splited[i])
-		i++;
-	if (i != 3)
-		exit (ft_print_error("- Colors configuration is not correct"));	
+	ft_check_fields(data->floor.splited);
+	ft_check_fields(data->ceiling.splited);
 }
