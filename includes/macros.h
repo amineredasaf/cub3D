e62acfb28f2ce37926d2aca33884098ed028f472
Amine Reda Saf <redasm39@gmail.com>
2022-09-02 14:09:31 +0100

    added start position and spawning orientation macros.

--- includes/macros.h
+++ includes/macros.h
@@ -6,7 +6,7 @@
 /*   By: rsaf <rsaf@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 21:32:33 by rsaf              #+#    #+#             */
-/*   Updated: 2022/09/01 17:36:21 by rsaf             ###   ########.fr       */
+/*   Updated: 2022/09/02 14:07:57 by rsaf             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # define TRUE 1
 # define FAlSE 0
 
-/*      sides       */
+/*				sides				*/
 # define S_NO 10
 # define S_SO 11
 # define S_WE 12
@@ -26,17 +26,26 @@
 # define S_F  14
 # define S_C  15
 
-/*      ft_isside flag  */
+/*				characters			*/
+
+# define C_WALL '1'
+# define C_EMPTY '0'
+# define C_NORTH 'N'
+# define C_SOUTH 'S'
+# define C_EAST 'E'
+# define C_WEST 'W'
+
+/* 				ft_isside flag		*/
 # define SIDE 1
 # define COLOR 0
 
-/*      MAP     */
+/*				MAP					*/
 # define W_FIRST_LAST 1
 # define W_INSIDE 2
 # define W_LAST 3
 
 
-/*     dubg */
+/*				dubg				*/
 
 # define HERE printf("was here\n");
 #endif // MACROS_H
