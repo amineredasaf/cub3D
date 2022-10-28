844a41fbb28ef7a5e30b5015b27ba2d9943125fb
Reda amine Saf <rsaf@e1r2p14.1337.ma>
2022-10-28 16:49:45 +0100

    leaks killer coding

--- parsing/leaks_killer.c
+++ parsing/leaks_killer.c
@@ -6,7 +6,7 @@
 /*   By: rsaf <rsaf@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 16:16:47 by rsaf              #+#    #+#             */
-/*   Updated: 2022/10/28 16:26:45 by rsaf             ###   ########.fr       */
+/*   Updated: 2022/10/28 16:49:36 by rsaf             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,15 @@ int kill_leaks(t_data *data, char *flag, char *mesg)
     int i;
 
     i = 0;
-    // system("leaks cub3d");
     while (flag[i])
     {
-            printf("done\n");
         if (flag[i] == 'F')
-        {
-            printf("F done\n");
             ft_free_split(data->file_content);
-        }
         if (flag[i] == 'T')
-        {
-            printf("T done\n");
             ft_free_split(data->textures);
-        }
         if (flag[i] == 'M')
-        {
-            printf("M done\n");
             ft_free_split(data->map_s.map);
-        }
         i++;
     }
-    printf("done\n");
     exit(ft_print_error(mesg));
 }
